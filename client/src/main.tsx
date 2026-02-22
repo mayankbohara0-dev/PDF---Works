28dee533da0e56f18932fc59018d9056618ae724
mayankbohara0-dev <mayankbohara0@gmail.com>
2026-02-22 15:08:48 +0530

    kjghf

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { HelmetProvider } from 'react-helmet-async'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 const queryClient = new QueryClient()
 
@@ -12,6 +13,7 @@ createRoot(document.getElementById('root')!).render(
     <HelmetProvider>
       <QueryClientProvider client={queryClient}>
         <App />
+        <SpeedInsights />
       </QueryClientProvider>
     </HelmetProvider>
   </StrictMode>,
